ca8a27c81f46a46b1cedac347e1979c528c8bd29
su8lim3kid <aldereteangel21@gmail.com>
2020-04-28 13:21:24 -0600

    updated navbar and home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,52 @@
 import React from 'react'
+import styled from 'styled-components'
+import tulum from './../images/tulum.jpg'
+import '../stylesheet/home.css'
 
-function Home() {
-  return (
-    <div>
-      Home
-    </div>
-  )
+
+
+
+class Home extends React.Component {
+  render() {
+    return (
+      <>
+      
+    <style>{'body { background-color: gray; }'}</style>
+           
+      <HImg src={tulum} alt='Tulum' /> 
+      <Header>
+        Welcome to Casa Denale
+      
+      </Header>
+  </>
+    )
+  }
 }
+ const Header = styled.div`
+  position: absolute;
+  left: 0;
+  top: 13%;
+  width: 100%;
+  text-align: center;
+  font-size: 40px;
+  color: white;
+  font-family:cursive;
+ 
+ `
+    const HImg = styled.img`
+    
+    width: 100vw;
+  position: relative;
+  left: 50%;
+  right: 50%;
+  margin-left: -50vw;
+  margin-right: -50vw;
+
+  `
+ 
+
+
+
+
 
 export default Home
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,46 +1,97 @@
-import React from 'react';
-import { Link, } from 'react-router-dom';
-import { Menu, } from "semantic-ui-react";
+import React from "react"
+import { NavLink, } from "react-router-dom"
+import { Menu, Segment, Sticky, } from 'semantic-ui-react'
+import Logo from './../images/MainLogo.png'
+import styled from 'styled-components'
 
-const Navbar = () => (
- <Menu>
-   <Link to="/">
-     <Menu.Item>
-       Home
-     </Menu.Item>
-   </Link>
-   <Link to="/about">
-     <Menu.Item>
-       About
-     </Menu.Item>
-   </Link>
-   <Link to="/gallery">
-     <Menu.Item>
-       Gallery
-     </Menu.Item>
-   </Link>
-   <Link to="/rooms">
-     <Menu.Item>
-       Rooms
-     </Menu.Item>
-   </Link>
-   <Link to="/booking">
-     <Menu.Item>
-       Booking
-     </Menu.Item>
-   </Link>
-   {/* <Link to="/policy">
-     <Menu.Item>
-       Policy
-     </Menu.Item>
-   </Link> */}
-   {/* policy will go on bottom nav */}
-   <Link to="/contact">
-     <Menu.Item>
-      Contact Us
-     </Menu.Item>
-   </Link>
- </Menu>
-)
 
-export default Navbar;
+const Navbar  = () =>
+
+
+<Sticky>
+  <Segment inverted>
+      <Menu inverted pointing secondary >
+        <Menu.Item >
+          <NavLink
+            exact
+            to="/"
+            activeStyle={styles.active}>
+            Home
+          </NavLink>
+        </Menu.Item>
+        <Menu.Item>
+          <NavLink
+            exact
+            to="/about"
+            activeStyle={styles.active}>
+            About
+          </NavLink>
+        </Menu.Item>
+        <Menu.Item>
+          <NavLink
+            to="/gallery"
+            activeStyle={styles.active}>
+            Gallery
+          </NavLink>
+        </Menu.Item>
+        <Menu.Item>
+          <NavLink
+            to="/rooms"
+            activeStyle={styles.active}>
+            Rooms
+          </NavLink>
+        </Menu.Item>
+        <Menu.Item>
+          <NavLink
+            to="/booking"
+            activeStyle={styles.active}>
+            Book Now
+          </NavLink>
+        </Menu.Item>
+        <Menu.Item>
+          <NavLink
+            to="/contact"
+            activeStyle={styles.active}>
+            Contact Us
+          </NavLink>
+        </Menu.Item>
+      </Menu>
+      <NavLink to="/">
+   <Img src={Logo} alt='logo' /> 
+   </NavLink>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <div style={{
+          display: 'flex',
+          justifyContent: 'center',
+          width: '50%',
+          
+        }}>
+      </div>
+      </div>  
+  
+        
+    </Segment>
+    </Sticky>
+   
+   
+
+
+
+  
+   const Img = styled.img`
+   position: absolute;
+   top: -4px;
+   right: 10px;
+   height: 100px;
+   width: 140px;
+ `
+      
+const styles = {
+  active: {
+    color: '#11D9DC',
+    fontWeight: 'bold',
+  }
+}
+export default Navbar
+
+
