a40b0503b25e34a26aa10f2b8ecc0d94a200077b
su8lim3kid <aldereteangel21@gmail.com>
2020-05-12 12:44:37 -0600

    grid on gallery

--- client/src/components/Booking.js
+++ client/src/components/Booking.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import  Container  from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 function Booking() {
+  
   return (
-    <div>
+    <>
+    <style>{'body { background-color: #fcf6e6; }'}</style>
      <p style = {{fontSize:'40px'}}> Booking Page Soon</p>
-    </div>
+
+     <Container>
+     <Row>
+    <Col>1 of 2</Col>
+    <Col>2 of 2</Col>
+  </Row>
+  <Row>
+    <Col>1 of 3</Col>
+    <Col>2 of 3</Col>
+    <Col>3 of 3</Col>
+  </Row>
+     </Container>
+    </>
   )
 }
 
--- client/src/components/Gallery.js
+++ client/src/components/Gallery.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import  Container  from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 function Gallery() {
   return (
-    <div>
-     <p style = {{fontSize:'40px'}}> Gallery Page Soon</p>
-    </div>
+    <>
+    <style>{'body { background-color: #fcf6e6; }'}</style>
+     <p style = {{fontSize:'40px'}}> Booking Page Soon</p>
+
+     <Container>
+     <Row>
+    <Col>1 of 2</Col>
+    <Col>2 of 2</Col>
+  </Row>
+  <Row>
+    <Col>1 of 3</Col>
+    <Col>2 of 3</Col>
+    <Col>3 of 3</Col>
+  </Row>
+     </Container>
+    </>
   )
 }
 
